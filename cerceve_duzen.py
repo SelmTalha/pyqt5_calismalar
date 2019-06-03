408e4e52602bb5eff54a177cd23758c89c2801e5
SelmTalha <selmtalha26@gmail.com>
2019-06-03 23:42:21 +0300

    PYQT5 ÇALIŞMALAR

---
+++ cerceve_duzen.py
@@ -0,0 +1,27 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+import sys
+
+def pencere():
+    app=QApplication(sys.argv)
+    pencere=QWidget()
+
+    h_box = QHBoxLayout() 
+
+    button1=QPushButton(pencere)
+    button1.setText("Ekle")
+
+    button2=QPushButton(pencere)
+    button2.setText("Ekle 2")
+
+    h_box.addWidget(button1)
+    h_box.addWidget(button2)
+    pencere.setLayout(h_box)
+
+    pencere.setGeometry(50,50,300,300)
+    pencere.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    pencere()
--- dialog.py
+++ dialog.py
@@ -5,19 +5,50 @@ import sys
 
 def pencere():
     app=QApplication(sys.argv)
-    pencere=QDialog()
-    button1=QPushButton(pencere)
+    pencere=QDialog() #Widget türü bir dialog değişkeni de pencere olsun.
+
+    #Button 1 Tanımlama:
+    button1=QPushButton(pencere) # Oluşturulan button'u pencere içine attık.
     button1.setText("Tıklama Alanı 1")
     button1.setGeometry(30,50,100,50)
+    button1.clicked.connect(buton1_tiklandi) #Tıklandığında buton1_tiklandi fonk'a git
 
+    #Button 2 Tanımlama:
     button2=QPushButton(pencere)
     button2.setText("Tıklama Alanı 2")
     button2.setGeometry(30,150,100,50)
+    button2.clicked.connect(buton2_tiklandi) #Click Event -> buton2_tiklandi fonk'a git
 
-    pencere.setGeometry(100,100,300,300)
-    pencere.show()
+    pencere.setGeometry(100,100,300,300) #dialog widgetinin boyunu ve konumunu ayarla.
+    pencere.show() #göster
+    pencere.setWindowTitle("Dialog Deneme")
 
-    sys.exit(app.exec_())
+    sys.exit(app.exec_()) 
 
-if __name__ == "__main__":
+def buton1_tiklandi():
+    print("Buton 1'e tıklandı")
+
+def buton2_tiklandi():
+    print("Buton 2'ye tıklandı")
+
+if __name__ == "__main__": #python dosyası main dosyasından(yazıldığı yerden) çağrıldıysa pencere fonk'unu getir!
     pencere()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
